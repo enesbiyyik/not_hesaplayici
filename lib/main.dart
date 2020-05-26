831a703255f4b6250177c4e6b716864c75aad033
enesbiyyik <enes.biyyik@gmail.com>
2020-05-26 22:46:23 +0300

    26.05.2020

--- lib/main.dart
+++ lib/main.dart
@@ -5,113 +5,136 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  String dersAdi;
+  int dersKredi = 1;
+  double dersHarfDegeri = 4;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Ortalama Hesapla"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: FloatingActionButton(onPressed: (){
+
+      },
+        child: Icon(Icons.add),),
+      body: uygulamaGovdesi(),
     );
+  }
+
+  Widget uygulamaGovdesi() {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.fromLTRB(20, 30, 20, 10),
+              color: Colors.pink.shade200,
+              child: Form(
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      onSaved: (kaydedilecek){
+                        dersAdi = kaydedilecek;
+                      },
+                      validator: (girilenDeger){
+                        if (girilenDeger.length > 0) return null;
+                        else return "Ders Adı boş bırakılamaz";
+                      },
+                      decoration: InputDecoration(
+                        labelText: "Ders Adı",
+                        hintText: "Ders Adını Giriniz",
+                        border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
+                      ),
+                    ),
+                    SizedBox(height: 30,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          child: DropdownButton<int>(items: dersKredileri(), value: dersKredi, onChanged: (secilenKredi){
+                            setState(() {
+                              dersKredi = secilenKredi;
+                            });
+                          },),
+                          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                          decoration: BoxDecoration(
+                            border: Border.all(width: 0.5),
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                          ),
+                        ),
+                        Container(
+                          child: DropdownButton<double>(items: dersHarfDegerleri(), value: dersHarfDegeri , onChanged: (secilenHarf){
+                            setState(() {
+                              dersHarfDegeri = secilenHarf;
+                            });
+                          }),
+                          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                          decoration: BoxDecoration(
+                            border: Border.all(width: 0.5),
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.green.shade200,
+              child: Text("Static Liste"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  List<DropdownMenuItem<int>> dersKredileri() {
+    List<DropdownMenuItem<int>> krediler = [];
+    for(int i = 1; i <= 10; i++){
+      krediler.add(DropdownMenuItem<int>(child: Text("$i Kredi", style: TextStyle(fontSize: 20),), value: i,));
+    }
+    return krediler;
+  }
+
+  List<DropdownMenuItem<double>> dersHarfDegerleri() {
+    List<DropdownMenuItem<double>> harfler = [];
+    harfler.add(DropdownMenuItem(child: Text(" AA", style: TextStyle(fontSize: 20),), value: 4,),);
+    harfler.add(DropdownMenuItem(child: Text(" BA", style: TextStyle(fontSize: 20),), value: 3.5,),);
+    harfler.add(DropdownMenuItem(child: Text(" BB", style: TextStyle(fontSize: 20),), value: 3,),);
+    harfler.add(DropdownMenuItem(child: Text(" CB", style: TextStyle(fontSize: 20),), value: 2.5,),);
+    harfler.add(DropdownMenuItem(child: Text(" CC", style: TextStyle(fontSize: 20),), value: 2,),);
+    harfler.add(DropdownMenuItem(child: Text(" DC", style: TextStyle(fontSize: 20),), value: 1.5,),);
+    harfler.add(DropdownMenuItem(child: Text(" DD", style: TextStyle(fontSize: 20),), value: 1,),);
+    harfler.add(DropdownMenuItem(child: Text(" FF", style: TextStyle(fontSize: 20),), value: 0,),);
+
+    return harfler;
   }
 }
